26a49b391cf08be14f4ca72fc329f37c3039f6e3
Nthabi23 <monagengn2@gmail.com>
2023-11-21 18:38:06 +0200

    App.js and App.css updates

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import Weather from "./Weather.js";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.css";
 
 function App() {
-  return (
+  return(
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Weather defaultCity="Pretoria" />
+      <footer>
+        <a href="">Open-source code</a> by Nthabiseng Monageng
+      </footer>
     </div>
   );
 }
