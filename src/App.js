808d47fcff171aed8cb2deb039941915042f4fad
Nthabi23 <monagengn2@gmail.com>
2023-11-24 13:41:17 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import "./App.css";
         <Weather defaultCity="Pretoria" />
         <footer>
           This project was coded by {" "}
-          <a href="" target="_blank" rel="noreferrer" className="footer-link">Nthabiseng Monageng</a> and is {" "} <a href="" target="_blank" rel="noreferrer" className="footer-link">open-source</a> on Github and hosted on {" "} <a href="" target="_blank" rel="noreferrer" className="footer-link">Netlify</a> 
+          <a href="https://www.linkedin.com/in/nthabiseng-monageng-115264185" target="_blank" rel="noreferrer" className="footer-link">Nthabiseng Monageng</a> and is {" "} <a href="https://github.com/Nthabi23/final-react-project" target="_blank" rel="noreferrer" className="footer-link">open-source</a> on Github 
         </footer>
       </div>
     </div>
